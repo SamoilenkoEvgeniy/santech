4197d2f94e35f8c629e967d27440b4e7a1365477
evgeniysamoilenko <samoilenkoevgeniy@gmail.com>
2018-03-05 22:51:12 +0300

    viewport fixed

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -2,7 +2,7 @@
 <head>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'/>
     <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'/>
-    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     <meta name="yandex-verification" content="{{ env("YANDEX_VERIFICATION") }}"/>
     <meta name="description" content="@yield("description")"/>
     <title>@yield("title")</title>
