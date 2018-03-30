c933ef63575afd8b458ba65ce0d61705c78c3b95
Evgeniy Samoylenko <Evgenii.Samoilenko@excelian.com>
2018-03-30 11:12:58 +0300

    telegram notifications

--- app/Modules/Base/Http/Controllers/WelcomeController.php
+++ app/Modules/Base/Http/Controllers/WelcomeController.php
@@ -20,11 +20,18 @@ class WelcomeController extends Controller
         ]);
     }
 
+    /**
+     * @return \Illuminate\Http\Response
+     */
     public function getModal()
     {
         return $this->view("base::modal");
     }
 
+    /**
+     * @param Request $request
+     * @return string
+     */
     public function submitOrder(Request $request)
     {
         $name = $request->input("name");
@@ -36,6 +43,10 @@ class WelcomeController extends Controller
 
         if (env("SMS_NOTIFICATION")) {
             file_get_contents("http://sms.ru/sms/send?api_id=" . env("SMS_NOTIFICATION_TOKEN") . "&text=" . urlencode(iconv("utf-8", "utf-8", "Новая заявка {$name} {$phone}")));
+        }
+
+        if (ENV("TELEGRAM_NOTIFICATION")) {
+            file_get_contents("https://api.telegram.org/bot" . ENV("TELEGRAM_TOKEN") . "&chat_id=" . ENV('TELEGRAM_CHAT_ID') . "&text=" . urlencode("Новая заявка {$name} {$phone}"));
         }
 
         return "<div class='header_modal'>Спасибо! Мы скоро свяжемся с вами!</div>";
