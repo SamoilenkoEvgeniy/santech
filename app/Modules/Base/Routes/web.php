5dfaffd0ac0b6b57b796960d56f5c175045e3d9a
evgeniysamoilenko <samoilenkoevgeniy@gmail.com>
2018-03-30 20:08:52 +0300

    routes fixes

--- app/Modules/Base/Routes/web.php
+++ app/Modules/Base/Routes/web.php
@@ -1,12 +1,3 @@
 <?php
 
-Route::group([], function () {
-    Route::get('/getModal', "WelcomeController@getModal");
-    Route::post('/submitOrder', "WelcomeController@submitOrder");
-    Route::get('/', "WelcomeController@index");
-});
 
-Route::group(["prefix" => "/admin/crud", "middleware" => "auth.admin"], function () {
-    Route::get('/settings', "SettingController@index");
-    Route::post('/settings/update', "SettingController@update");
-});
